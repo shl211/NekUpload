35971d4e8fa02fde30af41117153da9ebf34c1e8
shl21_linux <stephen.liu21@outlook.com>
2025-02-13 00:26:38 +0000

    Add better styling and grouping of elements for better UI

--- app.py
+++ app.py
@@ -7,9 +7,17 @@ class NekUploadGUI:
     def __init__(self,root: Tk) -> None:
         self.root: Tk = root
         self.mainframe: ttk.Frame = self._create_mainframe()
+        
+        #static fields
         self.title: StringVar = StringVar()
         self.publication_date: StringVar = StringVar()
+        self.is_api_key_exist: StringVar = StringVar()
+        self.is_host_name_exist: StringVar = StringVar()
+
+        #dynamic fields
         self.dirname: StringVar = StringVar()
+        self.dir_label: ttk.Label = None #created in dynamic fields frame
+
 
         self.create_header_frame()
         self.create_static_fields_frame()
@@ -31,38 +39,50 @@ class NekUploadGUI:
     def create_header_frame(self) -> None: 
         header_frame: ttk.Frame = ttk.Frame(self.mainframe) 
         header_frame.grid(column=0, row=0, columnspan=2,sticky=(N, W, E, S))
+        header_frame.columnconfigure(0, weight=1)
+        header_frame.columnconfigure(1, weight=1)
 
+        # add title and header
         style: ttk.Style = ttk.Style()
         style.configure("Header.TLabel", font=("Helvetica", 24, "bold"), underline=True)
-
         header: ttk.Label = ttk.Label(header_frame, text="Welcome to NekUpload", style="Header.TLabel")
-        header.grid(column=0, row=0, sticky=N)
+        header.grid(column=0, row=0,columnspan=2,sticky=(N))
 
+        # add some description/info for user
         description_text: str = "This is an interactive GUI upload and validation pipeline for Nektar++ datasets. " + \
             "Please complete the setup instructions found in the User Guide before proceeding [link]."
-
-        description: ttk.Label = ttk.Label(header_frame, text=description_text,wraplength=500)
-        description.grid(column=0, row=1,sticky=W)
+        description: ttk.Label = ttk.Label(header_frame, text=description_text,wraplength=600)
+        description.grid(column=0, row=1,columnspan=2,sticky=N)
 
     def create_static_fields_frame(self) -> None:
-        static_fields_frame: ttk.Frame = ttk.Frame(self.mainframe)
+        static_fields_frame: ttk.Labelframe = ttk.Labelframe(self.mainframe,text="Basic Info")
         static_fields_frame.grid(column=0, row=1, sticky=(N, W, E, S))
 
-        static_fields_header: ttk.Label = ttk.Label(static_fields_frame, text="Dataset Metadata")
-        static_fields_header.grid(column=0, row=0, columnspan=2, sticky=N)
+        #tick box to serve as reminder for user to set API key environment variable
+        api_key_check: ttk.Checkbutton = ttk.Checkbutton(static_fields_frame,text="Have you set environment variable NEKTAR_DB_API_KEY?",
+                                       command=None,variable=self.is_api_key_exist,
+                                       onvalue="True",offvalue="False",
+                                       )
+        api_key_check.grid(row=1,column=0,sticky=W,padx=5,pady=.5)
 
+        #tick box to serve as reminder for user to set host name environment variable
+        host_name_check: ttk.Checkbutton = ttk.Checkbutton(static_fields_frame,text="Have you set environment variable NEKTAR_DB_HOST?",
+                                       command=None,variable=self.is_host_name_exist,
+                                       onvalue="True",offvalue="False")
+        host_name_check.grid(row=2,column=0,sticky=W,padx=5,pady=.5)
+
+        #ask for title of the dataset
         title_label: ttk.Label = ttk.Label(static_fields_frame, text="Title: ")
-        title_label.grid(row=1, column=0, sticky=W, padx=5, pady=.5)
-
+        title_label.grid(row=4, column=0, sticky=W, padx=5, pady=.5)
         title_widget: ttk.Entry = ttk.Entry(static_fields_frame, textvariable=self.title) 
-        title_widget.grid(row=1, column=1, sticky=E, padx=5, pady=.5)
+        title_widget.grid(row=4, column=1, sticky=E, padx=5, pady=.5)
 
+        #ask for publication date, pre-populate with current date
         publication_date_label: ttk.Label = ttk.Label(static_fields_frame, text="Publication Date: ") 
-        publication_date_label.grid(row=2, column=0, sticky=W, padx=5, pady=.5)
-
+        publication_date_label.grid(row=5, column=0, sticky=W, padx=5, pady=.5)
         self.publication_date.set(self._get_current_iso8601_date())  
         publication_date_widget: ttk.Entry = ttk.Entry(static_fields_frame, textvariable=self.publication_date) 
-        publication_date_widget.grid(row=2, column=1, sticky=E, padx=5, pady=.5)
+        publication_date_widget.grid(row=5, column=1, sticky=E, padx=5, pady=.5)
 
     def _get_current_iso8601_date(self) -> str:
         today: date = date.today() 
@@ -75,22 +95,43 @@ class NekUploadGUI:
         placeholder_label: ttk.Label = ttk.Label(dynamic_fields_frame, text="PLACEHOLDER") 
         placeholder_label.grid(row=0, column=0, sticky=W)
 
-    def create_file_selector_frame(self) -> None:
-        file_selector_frame: ttk.Frame = ttk.Frame(self.mainframe) 
-        file_selector_frame.grid(column=0, row=2, columnspan=2, sticky=S)
+    def _create_directory_upload_frame(self,parent:ttk.Frame) -> ttk.Frame:
+        file_selector_frame: ttk.Frame = ttk.Frame(parent) 
+        file_selector_frame.grid(column=0, row=2, columnspan=2, sticky=(N,E,S,W))
 
         file_header_label: ttk.Label = ttk.Label(file_selector_frame, text="Select Dataset")
-        file_header_label.grid(column=0, row=0, sticky=N)
-
-        dir_label: ttk.Label = ttk.Label(file_selector_frame,text="Selected Directory",textvariable=self.dirname)
-        dir_label.grid(column=0,row=1,sticky=W)
+        file_header_label.grid(column=0, row=0, sticky=W)
 
         find_dir_button: ttk.Button = ttk.Button(
             file_selector_frame,
             text="Select Directory",
-            command=lambda: (self.dirname.set(filedialog.askdirectory()), print(f"dirname.get(): {self.dirname.get()}")) #
+            command=self._select_directory #
         )
         find_dir_button.grid(column=0, row=0, sticky=W)
+
+        self.dir_label: ttk.Label = ttk.Label(file_selector_frame,text=f"Selected Directory: None")
+        self.dir_label.grid(column=1,row=0,sticky=W)
+
+        return file_selector_frame
+
+    def create_file_selector_frame(self) -> None:
+        n = ttk.Notebook(self.mainframe)
+        f1 = self._create_directory_upload_frame(n)
+        f2 = ttk.Frame(n)
+        n.add(f1,text="Upload by Directory")
+        n.add(f2,text="Upload by Files")
+        n.grid(row=5,column=0)
+
+    def _select_directory(self) -> None:
+        selected_dir = filedialog.askdirectory()
+        if selected_dir:
+            self.dirname.set(selected_dir)
+            self.dir_label.config(text=f"Selected Directory: {selected_dir}")  # Update text
+            print(f"dirname.get(): {self.dirname.get()}")
+        else:
+            self.dirname.set("")
+            self.dir_label.config(text="Selected Directory: None")  # Update text
+            print("No directory selected")
 
     def submit_form(self) -> None: 
         print(self.title.get()) 
