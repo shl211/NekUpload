50511b1d93ab6e3bc86090a27c5876e803bd3caf
shl21_linux <stephen.liu21@outlook.com>
2025-03-07 14:54:25 +0000

    Add a way to auto extract certain basic details from the files

--- NekUpload/metadataModule/extractor.py
+++ NekUpload/metadataModule/extractor.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 import h5py
 import lxml
 import numpy as np
-from typing import Tuple
+from typing import Tuple,List,Dict
 
 class Extractor(ABC):
     def __init__(self):
@@ -51,6 +51,34 @@ class HDF5Extractor(Extractor):
             np.maximum(max_coord,max_coord_in_chunk,out=max_coord)
 
         return min_coord, max_coord
+
+class AutoExtractor:
+    def __init__(self,session_file: str,geometry_file: str,output_file: List[str]):
+        self.session_file = session_file
+        self.geometry_file = geometry_file
+        self.output_file = output_file
+
+    def extract_data(self) -> Dict[str,str]:
+        results = {}
+
+        with h5py.File(self.output_file) as f:
+            if version := HDF5Extractor.extract_attribute(f,"NEKTAR/Metadata/Provenance","NektarVersion"):
+                results["VERSION"] = version 
+            
+            if git_hash := HDF5Extractor.extract_attribute(f,"NEKTAR/Metadata/Provenance","GitSHA1"):
+                results["GITHASH"] = git_hash
+        
+        with h5py.File(self.geometry_file) as f:
+            min_coords1,max_coords1 = HDF5Extractor.extract_attribute(f,"NEKTAR/GEOMETRY/MESH/VERT")
+            min_coords2,max_coords2 = HDF5Extractor.extract_attribute(f,"NEKTAR/GEOMETRY/MESH/CURVE_NODES")
+
+            min_coords = np.minimum(min_coords1,min_coords2)
+            max_coords = np.maximum(max_coords1,max_coords2)
+
+            results["MAX_COORD"] = max_coords
+            results["MIN_COORD"] = min_coords
+
+        return results
 
 if __name__ == "__main__":
     with h5py.File("tests/datasets/ADRSolver/ADR_2D_TriQuad.fld") as f:
