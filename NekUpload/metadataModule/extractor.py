a73ca2607b2798913fabcc3e6146592f6adb076c
shl21_linux <stephen.liu21@outlook.com>
2025-03-07 14:20:44 +0000

    Add way to stream and acquire min and max coordinates from geometry

--- NekUpload/metadataModule/extractor.py
+++ NekUpload/metadataModule/extractor.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 import h5py
 import lxml
+import numpy as np
+from typing import Tuple
 
 class Extractor(ABC):
     def __init__(self):
@@ -23,8 +25,38 @@ class HDF5Extractor(Extractor):
 
         except Exception:
             return None
+
+    def extract_min_max_coords(geometry_file: h5py.File,dataset_path: str) -> Tuple[np.ndarray,np.ndarray]:
+        dataset: h5py.Dataset = geometry_file[dataset_path]
+        shape = dataset.shape
+
+        # Ensure dataset is at least 2D and has 3 columns
+        if len(shape) < 2 or shape[1] != 3:
+            raise ValueError(f"Expected a dataset with shape (N,3), but got {shape}")
+
+        #initialise
+        CHUNK_SIZE = 1000
+        min_coord = np.full(3, np.inf)
+        max_coord = np.full(3, -np.inf)
         
+        for chunk_start in range(0,shape[0],CHUNK_SIZE):
+            chunk_end = min(chunk_start + CHUNK_SIZE, shape[0])
+            data_chunk = dataset[chunk_start:chunk_end, :]
+
+            min_coord_in_chunk: np.ndarray = np.amin(data_chunk,axis=0)
+            max_coord_in_chunk: np.ndarray = np.amax(data_chunk,axis=0)
+
+            #in-place as memory allocations are expensive in Python
+            np.minimum(min_coord,min_coord_in_chunk,out=min_coord)
+            np.maximum(max_coord,max_coord_in_chunk,out=max_coord)
+
+        return min_coord, max_coord
+
 if __name__ == "__main__":
     with h5py.File("tests/datasets/ADRSolver/ADR_2D_TriQuad.fld") as f:
         v = HDF5Extractor.extract_attribute(f,"NEKTAR/Metadata/Provenance","NektarVersion")
         print(v)
+
+    with h5py.File("tests/datasets/ADRSolver/ADR_2D_TriQuad.nekg") as f:
+        coords = HDF5Extractor.extract_min_max_coords(f,"NEKTAR/GEOMETRY/MESH/VERT")
+        print(coords)
--- tests/unit/metadataModule/test_extractor.py
+++ tests/unit/metadataModule/test_extractor.py
@@ -1,6 +1,7 @@
 from NekUpload.metadataModule.extractor import HDF5Extractor
 import h5py
 import os
+import numpy as np
 
 def test_hdf5_attr_extractor():
 
@@ -18,7 +19,7 @@ def test_hdf5_attr_extractor():
 
     os.remove("testfile.h5")
 
-def test_hdf5_attr_extractor():
+def test_hdf5_attr_extractor_bad_group():
 
     # Create a new HDF5 file
     with h5py.File('testfile.h5', 'w') as f:
@@ -34,7 +35,7 @@ def test_hdf5_attr_extractor():
 
     os.remove("testfile.h5")
 
-def test_hdf5_attr_extractor():
+def test_hdf5_attr_extractor_bad_attribute():
 
     # Create a new HDF5 file
     with h5py.File('testfile.h5', 'w') as f:
@@ -47,5 +48,33 @@ def test_hdf5_attr_extractor():
     with h5py.File('testfile.h5','r') as f:
         val = HDF5Extractor.extract_attribute(f,"test_group","invalid_attribute")
         assert not val
+
+    os.remove("testfile.h5")
+
+def test_hdf5_attr_geometry_streaming():
+
+    # Create a new HDF5 file
+    with h5py.File('testfile.h5', 'w') as f:
+        # Create a group in the file
+        grp = f.create_group('test_group')
+        # Create a dataset with 1000 coordinates with random numbers between 0 and 100
+        data = np.random.randint(0, 100, size=(1000, 3))
+        
+        #make sure min is 0 and max is 100
+        data[400][0] = 0
+        data[304][1] = 0
+        data[999][2] = 0
+
+        data[123][0] = 100
+        data[2][1] = 100
+        data[555][2] = 100
+
+        grp.create_dataset('coordinates', data=data)
+
+
+    with h5py.File('testfile.h5','r') as f:
+        min_coord,max_coord = HDF5Extractor.extract_min_max_coords(f,"test_group/coordinates")
+        assert np.array_equal(min_coord, np.array([0,0,0]))
+        assert np.array_equal(max_coord,np.array([100,100,100]))
 
     os.remove("testfile.h5")
