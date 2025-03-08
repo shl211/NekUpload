ae39d551fdfe7f912709eb5ad09645d924ca59e4
shl21_linux <stephen.liu21@outlook.com>
2025-03-08 10:27:32 +0000

    Add a page with some layout guidance

--- NekUpload/index.py
+++ NekUpload/index.py
@@ -3,7 +3,7 @@ from ttkbootstrap.constants import *
 #from NekUpload.newFrontend.scenes
 from NekUpload.newFrontend.components.header import Header
 from NekUpload.newFrontend.components.menu import Menu
-
+from NekUpload.newFrontend.scenes.info import InfoScene
 class NekUploadNewGUI:
     def __init__(self):
         self.root = ttk.Window(themename="litera")
@@ -17,11 +17,21 @@ class NekUploadNewGUI:
 
         #create a header
         self.header = Header(self.root)
-        self.header.grid(row=0,column=0,padx=20,pady=20,columnspan=10,sticky=(N,E,S,W))
+        self.header.grid(row=0,column=0,padx=20,pady=20,columnspan=2,sticky=(N,E,S,W))
 
         #create a menu
         self.menu = Menu(self.root)
-        self.menu.grid(row=1,column=0,padx=10,pady=10,rowspan=10,sticky=NSEW)
+        self.menu.grid(row=1,column=0,padx=10,pady=10,sticky=NSEW)
+
+        #create page frame
+        #menu buttons will direct pages to here
+        self.page = InfoScene(self.root)
+        self.page.grid(row=1,column=1,padx=20,pady=10,sticky=NSEW)
+
+        #help with visualising for now
+        self.header.config(style="primary.TFrame")  # Bootstrap theme color
+        self.menu.config(style="danger.TFrame")  # Another color for menu
+        self.page.config(style="success.TFrame")  #
 
     def run(self):
         self.root.mainloop()
--- NekUpload/newFrontend/scenes/info.py
+++ NekUpload/newFrontend/scenes/info.py
@@ -1,6 +1,22 @@
-class InfoScene:
-    def __init__(self):
-        pass
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+class InfoScene(ttk.Frame):
+    def __init__(self,parent):
+        super().__init__(parent)
 
-    
+        self.rowconfigure(0,weight=1)
+        self.rowconfigure(1,weight=1)
+        self.rowconfigure(2,weight=1)
+        self.columnconfigure(0,weight=1)
+        self.columnconfigure(1,weight=1)
+        self.columnconfigure(2,weight=1)
 
+        self.placeholder = ttk.Label(
+            master=self,  # Ensure label is inside this frame
+            text="PLACEHOLDER",
+            font=("TkDefaultFont", 20),  # Make text larger for visibility
+            anchor="center"  # Center text inside the label
+        )
+
+        self.placeholder.grid(row=1,column=1,sticky=(NSEW))
+        
