77542eb3b4fc8d72dfe8a5134b1a70e5a462f8e4
shl21_linux <stephen.liu21@outlook.com>
2025-03-09 15:23:05 +0000

    Add way to extract API key depending on user input

--- NekUpload/newFrontend/scenes/settings.py
+++ NekUpload/newFrontend/scenes/settings.py
@@ -1,7 +1,9 @@
 import ttkbootstrap as ttk
 import tkinter as tk
 from ttkbootstrap.constants import *
+from ttkbootstrap.toast import ToastNotification
 import os
+
 class SettingScene(ttk.Frame):
     def __init__(self,parent):
         super().__init__(parent)
@@ -13,6 +15,28 @@ class SettingScene(ttk.Frame):
 
         self.api_key_info: ttk.LabelFrame = self._get_api_key(self)
         self.api_key_info.grid(row=1,column=0,padx=10,pady=10,sticky=NSEW)
+
+        #add a debug button for now
+        self.debug_button = ttk.Button(
+            master=self,
+            text="DEBUG",
+            command=self.debug_notify
+        )
+        self.debug_button.grid(row=2,column=0,sticky=E)
+
+    def debug_notify(self,event:tk.Event=None):
+        """Utility debugger
+
+        Args:
+            event (tk.Event, optional): _description_. Defaults to None.
+        """
+        toast = ToastNotification(
+            title="Check api message",
+            message=f"API KEY: {self.api_key}",
+            duration=3000,
+            position=(100,100,'se')
+        )
+        toast.show_toast()
 
     def _get_repository_info(self,parent) -> ttk.LabelFrame:
         frame = ttk.LabelFrame(
@@ -59,11 +83,18 @@ class SettingScene(ttk.Frame):
             text="API Key"
         )
 
+        #for formatting purposes, temporary for now
+        frame.grid_columnconfigure(0,minsize=250,weight=1)
+        frame.grid_columnconfigure(1,minsize=25,weight=1)
+        frame.grid_columnconfigure(2,minsize=25,weight=1)
+        frame.grid_columnconfigure(3,minsize=25,weight=1)
+        frame.grid_columnconfigure(4,minsize=25,weight=1)
+
         radio_label = ttk.Label(
             master=frame,
             text="I have: "
         )
-        radio_label.grid(row=1, column=0, padx=5, pady=5, sticky=W)
+        radio_label.grid(row=1, column=0, padx=5, pady=5, sticky=EW)
 
         self._api_radio_value = tk.StringVar(value="Option1")
         radio_option1 = ttk.Radiobutton(
@@ -73,7 +104,7 @@ class SettingScene(ttk.Frame):
             variable=self._api_radio_value,
             command=self.update_api_form
         )
-        radio_option1.grid(row=1, column=1, padx=5, pady=5, sticky=W)
+        radio_option1.grid(row=1, column=1, padx=5, pady=5, sticky=EW)
 
         radio_option2 = ttk.Radiobutton(
             master=frame,
@@ -95,11 +126,11 @@ class SettingScene(ttk.Frame):
             master=frame,
             textvariable=self._api_key
         )
-        api_key_entry.grid(row=2, column=1,columnspan=2, padx=5, pady=5, sticky=W)
+        api_key_entry.grid(row=2, column=1,columnspan=2, pady=5, sticky=EW)
 
         return frame
 
-    def update_api_form(self,event=None):
+    def update_api_form(self,event: tk.Event=None):
         if self._api_radio_value.get() == "Option1":
             self.api_key_label.configure(text="API Key: ")
         elif self._api_radio_value.get() == "Option2":
@@ -144,3 +175,11 @@ class SettingScene(ttk.Frame):
     def host_url(self,value: str):
         self._host_url.set(value)
 
+    @property
+    def api_key(self):
+        if self._api_radio_value.get() == "Option1":
+            #if given as plain key
+            return self._api_key.get()
+        elif self._api_radio_value.get() == "Option2":
+            #if given as environment variable
+            return os.getenv(self._api_key.get(),None)
